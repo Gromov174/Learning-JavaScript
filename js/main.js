1e2975ae925a4cecc49c785816389d47ef9f6d08
Gromov174 <GromovAlexandr174@gmail.com>
2023-10-25 19:07:52 +0500

    five homework js

--- js/main.js
+++ js/main.js
@@ -154,86 +154,134 @@
 
 // =====Четвертая домашняя работа=====
 
-const questionMovie = [
-  'Один и последних просмотренных фильмов?',
-  'На сколько оцените его?',
-],
-  personalMovieDB = {
-    count: null,
-    movies: {},
-    actors: {},
-    genres: [],
-    privat: true,
-    start: () => {
-      personalMovieDB.count = +prompt('Сколько фильмов Вы уже посмотрели?');
-      while (personalMovieDB.count == null || personalMovieDB.count == '' || isNaN(personalMovieDB.count)) {
-        personalMovieDB.count = +prompt('Сколько фильмов Вы уже посмотрели?');
-      }
-    },
-    askQuestions: () => {
-      for (let i = 0; i < 2; i++) {
-        for (let i = 0; i < questionMovie.length; i++) {
-          let answer = prompt(questionMovie[i]);
+// const questionMovie = [
+//   'Один и последних просмотренных фильмов?',
+//   'На сколько оцените его?',
+// ],
+//   personalMovieDB = {
+//     count: null,
+//     movies: {},
+//     actors: {},
+//     genres: [],
+//     privat: false,
+//     start: () => {
+//       personalMovieDB.count = +prompt('Сколько фильмов Вы уже посмотрели?');
+//       while (personalMovieDB.count == null || personalMovieDB.count == '' || isNaN(personalMovieDB.count)) {
+//         personalMovieDB.count = +prompt('Сколько фильмов Вы уже посмотрели?');
+//       }
+//     },
+//     askQuestions: () => {
+//       for (let i = 0; i < 2; i++) {
+//         for (let i = 0; i < questionMovie.length; i++) {
+//           let answer = prompt(questionMovie[i]);
 
-          if (answer == null || answer == '' || answer.length > 50) {
-            i--;
-          } else {
-            answers.push(answer);
-          };
-        };
-      }
-      userFilms[answers[0]] = `${answers[1]} из 10`;
-      userFilms[answers[2]] = `${answers[3]} из 10`;
-    },
-    writeYourGenres: () => {
-      for (let i = 0; i < 3; i++) {
-        const favoriteGenres = prompt(`Ваш любимый жанр под номером ${i + 1}`);
-        if (favoriteGenres == null || favoriteGenres == '' || favoriteGenres.length > 50) {
-          i--
-        } else {
-          personalMovieDB.genres[i] = favoriteGenres;
-        }
-      }
-      personalMovieDB.genres.forEach((e, i) => {
-        console.log(`Любимый жанр #${i + 1} - это ${e.toLowerCase()}`);
-      })
-    },
-    setLvlUser: () => {
-      if (personalMovieDB.count < 10) {
-        alert('Вы посмотрели довольно мало фильмов')
-      } else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
-        alert('Вы классический зритель')
-      } else if (personalMovieDB.count > 30) {
-        alert('Вы киноман!')
-      } else {
-        alert('Произошла ошибка')
-      };
-    },
-    toggleVisibleMyDB: () => {
-      if (personalMovieDB.privat === false) {
-        personalMovieDB.privat = true;
-      } else {
-        personalMovieDB.privat = false;
-      }
-    },
-    showMyDB: () => {
-      if (personalMovieDB.privat == false) {
-        console.log(personalMovieDB)
-      } else {
-      }
-    }
-  },
-  userFilms = personalMovieDB.movies,
-  answers = [];
+//           if (answer == null || answer == '' || answer.length > 50) {
+//             i--;
+//           } else {
+//             answers.push(answer);
+//           };
+//         };
+//       }
+//       userFilms[answers[0]] = `${answers[1]} из 10`;
+//       userFilms[answers[2]] = `${answers[3]} из 10`;
+//     },
+//     writeYourGenres: () => {
+//       for (let i = 0; i < 3; i++) {
+//         const favoriteGenres = prompt(`Ваш любимый жанр под номером ${i + 1}`).toLowerCase();
+//         if (favoriteGenres == null || favoriteGenres == '' || favoriteGenres.length > 50) {
+//           i--
+//         } else {
+//           personalMovieDB.genres[i] = favoriteGenres;
+//         }
+//       }
+//       personalMovieDB.genres.forEach((e, i) => {
+//         console.log(`Любимый жанр #${i + 1} - это ${e}`);
+//       })
+//     },
+//     setLvlUser: () => {
+//       if (personalMovieDB.count < 10) {
+//         alert('Вы посмотрели довольно мало фильмов')
+//       } else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+//         alert('Вы классический зритель')
+//       } else if (personalMovieDB.count > 30) {
+//         alert('Вы киноман!')
+//       } else {
+//         alert('Произошла ошибка')
+//       };
+//     },
+//     toggleVisibleMyDB: () => {
+//       if (personalMovieDB.privat) {
+//         personalMovieDB.privat = false;
+//       } else {
+//         personalMovieDB.privat = true;
+//       }
+//     },
+//     showMyDB: () => {
+//       if (personalMovieDB.privat) {
 
-personalMovieDB.start()
+//       } else {
+//         console.log(personalMovieDB)
+//       }
+//     }
+//   },
+//   userFilms = personalMovieDB.movies,
+//   answers = [];
 
-personalMovieDB.askQuestions()
+// personalMovieDB.start()
 
-personalMovieDB.writeYourGenres()
+// personalMovieDB.askQuestions()
 
-personalMovieDB.setLvlUser();
+// personalMovieDB.writeYourGenres()
 
-personalMovieDB.toggleVisibleMyDB();
+// personalMovieDB.setLvlUser();
 
-personalMovieDB.showMyDB();
+// // personalMovieDB.toggleVisibleMyDB();
+
+// personalMovieDB.showMyDB();
+
+// =====Пятая домашняя работа=====
+
+// const x = [] + false - null + true;
+
+// =====Пятая домашняя работа=====
+
+// const adv = document.querySelectorAll('.promo__adv img'),
+//     poster = document.querySelector('.promo__bg'),
+//     genreName = poster.querySelector('.promo__genre'),
+//     films = document.querySelectorAll('.promo__interactive-item');
+//     // filmList = document.querySelector('.promo__interactive-list');
+
+// adv.forEach(e => {
+//     e.remove()
+// })
+
+// genreName.textContent = 'драма'
+
+// poster.style.background = 'url(../img/bg.jpg)';
+
+// const movieDB = {
+//     movies: [
+//         "Логан",
+//         "Лига справедливости",
+//         "Ла-ла лэнд",
+//         "Одержимость",
+//         "Скотт Пилигрим против..."
+//     ]
+// },
+//     { movies } = movieDB;
+
+// movies.sort();
+
+// films.forEach((name, i) => {
+//     name.textContent = `${i + 1} ${movies[i]}`
+// })
+
+// // filmList.innerHTML = null;
+
+// // movies.forEach((name, i) => {
+// //     filmList.innerHTML += `
+// //     <li class="promo__interactive-item">${i + 1} ${name}
+// //             <div class="delete"></div>
+// //     </li>
+// //     `
+// // });
